f1a894e273212d54ac303c81a2d96096e2d47c1c
Andy Miller <amiller@objectpartners.com>
2013-03-23 19:05:36 -0500

    updated the description.

--- src/ValidateConfigGrailsPlugin.groovy
+++ src/ValidateConfigGrailsPlugin.groovy
@@ -2,34 +2,17 @@ import com.onetribeyoyo.util.ConfigUtils
 
 class ValidateConfigGrailsPlugin {
 
-    def version = "0.1"
+    def version = "0.3"
     def grailsVersion = "2.0 > *"
     def title = "Validate Config Plugin"
     def author = "Andy Miller"
     def authorEmail = "onetribeyoyo@gmail.com"
     def description = '''\
-Adds methods to ConfigObject for validating expected and required properties.  This is especially useful when the config has been externalized.
-
-To use it simply add something like this to Config.groovy
-validate  {
-    required = [ "a", "b", "c" ]
-    expected = [ "p":123, "d":"foobar", "q":"/dev/null" ]
-}
-
-Then, usually in BootStrap, calling
-
-    grailsApplication.config.validateExpectedProperties()
-
-will set defaults for missing expected properties. and calling
-
-    grailsApplication.config.validateRequiredProperties()
-
-will throw ConfigurationException for missing required properties.
-
-See the sample app at https://github.com/onetribeyoyo/validate-config/tree/master/example .
+Adds methods to ConfigObject for validating expected and required properties.
+To use it, specify validate.required and validate.expected in Config.groovy
 '''
 
-    def documentation = "http://grails.org/plugin/validate-config"
+    def documentation = "https://github.com/onetribeyoyo/validate-config"
 
     def license = "APACHE"
     def issueManagement= [ system: "github", url: "https://github.com/onetribeyoyo/validate-config/issues" ]
